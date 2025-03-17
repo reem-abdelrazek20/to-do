f5a9e7a8bdb8dcd3fa3fbc0079c7da3517d8cfa1
reem-abdelrazek20 <156788337+reem-abdelrazek20@users.noreply.github.com>
2025-03-17 17:57:08 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,3 +1,22 @@
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+  themeToggle.checked = true; 
+}
+
+
+themeToggle.addEventListener("change", () => {
+  if (themeToggle.checked) {
+    body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark"); 
+  } else {
+    body.classList.remove("dark-mode"); 
+    localStorage.setItem("theme", "light"); 
+  }
+});
 
 
 const input = document.querySelector(".input");
@@ -6,7 +25,7 @@ const add = document.querySelector(".add");
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 
-// منع السلوك الافتراضي للنموذج
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -16,14 +35,19 @@ form.addEventListener("submit", (e) => {
     }
 
     const li = document.createElement("li");
-    li.classList.add("flex", "justify-between", "items-center", "p-4", "mb-2");
+    li.classList.add("flex", "justify-between", "items-center", "p-4", "mb-2", 'bg-[#3a3a553b]');
+
+   
+   
+ 
+
 
     const div = document.createElement("div");
     div.classList.add("flex", "items-center");
 
     const check = document.createElement("input");
     check.type = "checkbox";
-    check.classList.add("mr-4", "w-5", "h-5");
+    check.classList.add("mr-4", "w-5", "h-5","cursor-pointer",'check');
 
     const p = document.createElement("p");
     p.textContent = input.value;
@@ -33,7 +57,11 @@ form.addEventListener("submit", (e) => {
 
     const exst = document.createElement("button");
     exst.textContent = "×";
-    exst.classList.add("ml-4", "exst", "font-bold", "cursor-pointer", "text-xl");
+    exst.classList.add("bg-red-500", "border-none" ,"text-white" ,"px-4" ,"py-1" ,"rounded" ,"cursor-pointer");
+   
+
+  
+
 
     exst.addEventListener("click", () => {
         li.remove();
@@ -52,7 +80,7 @@ form.addEventListener("submit", (e) => {
 
         const finch = document.createElement("p");
         finch.textContent = "Well Done!";
-        finch.classList.add("text-center", "pt-[71px]", "text-[30px]", "text-[#24d524]" ,"uppercase", "tracking-[10px]" ,"font-extrabold");
+        finch.classList.add("text-center", "pt-[71px]", "text-[30px]", "text-[#24d524]" ,"uppercase", "font-extrabold");
 
        
         message.appendChild(finch);
@@ -63,3 +91,45 @@ form.addEventListener("submit", (e) => {
         }, 1000);
     });
 });
+
+    
+
+function updateDate() {
+    const today = new Date();
+    
+   
+
+    
+    const formattedDate = ` ${today.getDate()} /${today.getMonth()+1} /${today.getFullYear()}`;
+    
+    document.getElementById("dateDisplay").textContent =formattedDate;
+}
+
+
+
+function updateClock() {
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    
+    hours = hours % 12 || 12; 
+
+  
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    const timeString = `${hours}:${minutes}:${seconds} `;
+
+    document.getElementById("clock").textContent = timeString;
+}
+
+
+setInterval(updateClock, 1000);
+
+
+window.onload =function () {
+   updateDate() ;
+   updateClock();  
+}
